0d8af263d65507df311c86645187217c464fd2d9
keerthi4921 <keerthia729@gmail.com>
2026-01-31 14:27:48 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+    public static void main(String[] args){
+    System.out.println("Good afternoon");
+    System.out.println("Welcome to java class");
+    System.out.print("Cse-a");
+    System.out.println("MTIEAT");
+}
+}
