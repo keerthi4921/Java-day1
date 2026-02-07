f844e564a5bd8ff6d0ddd84d0131840d880bf47c
keerthi4921 <keerthia729@gmail.com>
2026-02-07 10:55:34 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,20 @@
+public class program {
+    public static void main(String[] args) {
+        String studentname="Maya";
+        int age=18;
+        char ch='B';
+        short marks=76;
+        String status="Pass";
+        System.out.println(studentname);
+        System.out.println(age);
+        System.out.println(ch);
+         System.out.println(marks);
+          System.out.println(status);
+
+
+
+
+
+    }
+    
+}
