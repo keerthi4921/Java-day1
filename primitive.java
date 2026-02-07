7682f5771d3c6c83dda6b2e1313e0e702b211811
keerthi4921 <keerthia729@gmail.com>
2026-02-07 10:55:18 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,21 @@
+public class primitive {
+    public static void main(String[] args) {
+        byte a=120;
+        short b=188;
+        int c=20917;
+        long d=100000000;
+        float f=2.77f;
+        double de=67.23356;
+        char ch='#';
+        boolean value=false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(de);
+        System.out.println(ch);
+        System.out.println(value);
+    }
+    
+}
