6cd87cddb93009fa405f5e713a56c136dbf8779f
keerthi4921 <keerthia729@gmail.com>
2026-02-07 11:11:15 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,16 @@
+public class details {
+    public static void main(String[] args) {
+        String a="Sirai";
+        byte b=7;
+        int c=150;
+        boolean value=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(value);
+        
+        
+
+    }
+    
+}
