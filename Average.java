5bd4b2532a33fc188890f74fc6ea1ff8c234ee8a
keerthi4921 <keerthia729@gmail.com>
2026-02-07 10:54:52 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,11 @@
+public interface Average {
+    public static void main(String args[]){
+        int subject1=94;
+        int subject2=99;
+        int subject3=78;
+        int total= subject1+subject2+subject3;
+        float average=subject1+subject2+subject3/3;
+        System.out.println(total);
+        System.err.println(average);
+    }
+}
